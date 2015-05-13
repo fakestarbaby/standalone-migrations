13150c8d39e2d63c7d5903933944c2af2b2067e0
fakestarbaby <fakestarbaby@gmail.com>
2015-05-14 08:41:11 +0900

    Add; NOT NULL

--- db/migrate/20150513130801_create_blogs.rb
+++ db/migrate/20150513130801_create_blogs.rb
@@ -1,8 +1,8 @@
 class CreateBlogs < ActiveRecord::Migration
   def change
     create_table :blogs do |t|
-      t.string :title
-      t.text :body
+      t.string :title, null: false
+      t.text :body, null: false
     end
   end
 end
