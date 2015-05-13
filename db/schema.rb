7e0885d3036529aeab595aaaa3c7823feb5f5fa8
fakestarbaby <fakestarbaby@gmail.com>
2015-05-14 08:46:24 +0900

    rake db:rollback

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513130801) do
-
-  create_table "blogs", force: :cascade do |t|
-    t.string   "title",      limit: 255,   null: false
-    t.text     "body",       limit: 65535, null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
