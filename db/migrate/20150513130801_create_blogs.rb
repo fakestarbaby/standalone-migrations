927c01b64d262209e12ec38fa72f5a2dca115d15
fakestarbaby <fakestarbaby@gmail.com>
2015-05-14 09:16:35 +0900

    Add; t.decimal :score, precision: 6, scale: 3, null: true

--- db/migrate/20150513130801_create_blogs.rb
+++ db/migrate/20150513130801_create_blogs.rb
@@ -3,6 +3,7 @@ class CreateBlogs < ActiveRecord::Migration
     create_table :blogs do |t|
       t.integer :user_id, limit: 8, null: false
       t.boolean :type, null: false
+      t.decimal :score, precision: 6, scale: 3, null: true
       t.date :date, null: false
       t.string :title, null: false
       t.text :body, null: false
