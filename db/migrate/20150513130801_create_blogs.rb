60ea29c8c433031fe7efd16bbeca5b789dedb37c
fakestarbaby <fakestarbaby@gmail.com>
2015-05-14 09:08:24 +0900

    Add; t.boolean :type, null: false

--- db/migrate/20150513130801_create_blogs.rb
+++ db/migrate/20150513130801_create_blogs.rb
@@ -2,6 +2,7 @@ class CreateBlogs < ActiveRecord::Migration
   def change
     create_table :blogs do |t|
       t.integer :user_id, limit: 8, null: false
+      t.boolean :type, null: false
       t.date :date, null: false
       t.string :title, null: false
       t.text :body, null: false
